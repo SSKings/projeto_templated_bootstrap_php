08c2c9905ec347f5201d2e27245e756d330dba8f
sskings <sergiosantanadosreis@hotmail.com>
2023-11-03 22:32:09 -0300

    página principal e verificar acesso

---
+++ verificarAcesso.php
@@ -0,0 +1,10 @@
+<?php
+    session_start();
+
+    if(!isset($_SESSION['logado'])){
+        echo ("<h2>Acesso Ao Sistema Não Permitido</h2>");
+        echo ("É necessário informar usuário e senha válidos para ter acesso.<br><br>");
+        echo ("Para entrar no sistema clique no botão abaixo:<br><br>");
+        echo ("<input type=\"button\" value=\"Login\" onclick=\"location.href='login.php'\" />");
+        die();
+    }
