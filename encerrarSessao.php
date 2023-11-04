ee0757de700d2d7daf8896da5ea634b4d8e8b804
sskings <sergiosantanadosreis@hotmail.com>
2023-11-03 22:32:55 -0300

    encerrar sessão

---
+++ encerrarSessao.php
@@ -0,0 +1,14 @@
+<?php
+
+include 'VerificarAcesso.php';
+
+session_start();
+session_unset();
+session_destroy();
+
+echo "<h2>Encerramento de Sessão de Sistema</h2>";
+
+echo ("Encerramento de Sessão realizado com sucesso.<br><br>");
+
+echo ("Para entrar no sistema novamente clique no botão abaixo:<br><br>");
+echo ("<input type=\"button\" value=\"Acesso\" onclick=\"location.href='login.php'\" />");
